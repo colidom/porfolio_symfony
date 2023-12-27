7bf0bb848622341a4f05b980f9862dd36572da92
colidom <colidom@outlook.com>
2023-12-27 20:41:22 +0100

    Añadidos proyectos desde php y mostrados en template

--- src/Controller/ProjectsController.php
+++ src/Controller/ProjectsController.php
@@ -11,8 +11,27 @@ class ProjectsController extends AbstractController
     #[Route('/projects', name: 'projects')]
     public function index(): Response
     {
+        $projects = [
+            [
+                'title' => 'Card title 1',
+                'image' => 'https://mdbcdn.b-cdn.net/img/new/standard/nature/110.webp',
+                'description' => "Some quick example text for the first card."
+            ],
+            [
+                'title' => 'Card title 2',
+                'image' => 'https://mdbcdn.b-cdn.net/img/new/standard/nature/111.webp',
+                'description' => "Some quick example text for the second card."
+            ],
+            [
+                'title' => 'Card title 3',
+                'image' => 'https://mdbcdn.b-cdn.net/img/new/standard/nature/112.webp',
+                'description' => "Some quick example text for the third card."
+            ]
+        ];
+
         return $this->render('projects/projects.html.twig', [
-            'title' => 'Proyectos'
+            'title' => 'Proyectos',
+            'projects' => $projects
         ]);
     }
 }
