67bf0c6a76a98090192c1779089c51e31aa79a56
colidom <colidom@outlook.com>
2024-01-02 16:56:16 +0100

    Ajustes en vista Proyectos
    Modificado bucle para mostrar un card que ocupe 2 card dependiendo de la iteración en la que me encuentre en ese momento

--- src/Controller/ProjectsController.php
+++ src/Controller/ProjectsController.php
@@ -13,18 +13,43 @@ class ProjectsController extends AbstractController
     {
         $projects = [
             [
-                'title' => 'Card title 1',
+                'title' => 'Card title 0',
                 'image' => 'https://mdbcdn.b-cdn.net/img/new/standard/nature/110.webp',
                 'description' => "Some quick example text for the first card."
             ],
             [
-                'title' => 'Card title 2',
+                'title' => 'Card title 1',
                 'image' => 'https://mdbcdn.b-cdn.net/img/new/standard/nature/111.webp',
                 'description' => "Some quick example text for the second card."
             ],
             [
-                'title' => 'Card title 3',
+                'title' => 'Card title 2',
                 'image' => 'https://mdbcdn.b-cdn.net/img/new/standard/nature/112.webp',
+                'description' => "Some quick example text for the third card."
+            ],
+            [
+                'title' => 'Card title 3',
+                'image' => 'https://mdbcdn.b-cdn.net/img/new/standard/nature/113.webp',
+                'description' => "Some quick example text for the third card."
+            ],
+            [
+                'title' => 'Card title 4',
+                'image' => 'https://mdbcdn.b-cdn.net/img/new/standard/nature/114.webp',
+                'description' => "Some quick example text for the third card."
+            ],
+            [
+                'title' => 'Card title 5',
+                'image' => 'https://mdbcdn.b-cdn.net/img/new/standard/nature/115.webp',
+                'description' => "Some quick example text for the third card."
+            ],
+            [
+                'title' => 'Card title 7',
+                'image' => 'https://mdbcdn.b-cdn.net/img/new/standard/nature/116.webp',
+                'description' => "Some quick example text for the third card."
+            ],
+            [
+                'title' => 'Card title 6',
+                'image' => 'https://mdbcdn.b-cdn.net/img/new/standard/nature/117.webp',
                 'description' => "Some quick example text for the third card."
             ]
         ];
