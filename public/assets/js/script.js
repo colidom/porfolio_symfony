b304e08f65663225ba4ae537c97ee67adf462ec9
colidom <colidom@outlook.com>
2024-01-02 16:43:03 +0100

    Reordenando recursos
    Movidos estilos y js a sus ficheros correspondientes para dar una mejor organización al proyecto

---
+++ public/assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".card-img-top").hover(
+        function () {
+            $(this).css("transform", "scale(1.1)");
+        },
+        function () {
+            $(this).css("transform", "scale(1)");
+        }
+    );
+});
