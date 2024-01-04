9c6fe8f4c51937dc1585be8f80a929fe439857c3
colidom <colidom@outlook.com>
2024-01-05 00:08:22 +0100

    Añadidas dos nuevas experiencias

--- src/Controller/CareerController.php
+++ src/Controller/CareerController.php
@@ -32,6 +32,20 @@ class CareerController extends AbstractController
                 'position' => 'Desarrollador de eCommerce en Atos IT Solutions and Services A/S',
                 'description' => 'Desarrollo de módulo/plugin e-commerce en las plataformas PrestaShop y Woocommerce para el cliente Correos, integrando servicios de paquetería nacional e internacional. Tecnologías empleadas: PHP, JavaScript, Python, SQL, Servicios REST y SOAP, HTML5, CSS3, jQuery y Bootstrap.',
                 'skills' => 'JavaScript, PHP'
+            ],
+            [
+                'date' => 'ago. 2019 - jun. 2021',
+                'location' => '38612 Granadilla, Santa Cruz de Tenerife',
+                'position' => 'Desarrollador SOA en el Centro de Excelencia para el desarrollo y la innovación CEDeI',
+                'description' => 'Desarrollador SOA (Arquitectura Orientada a Servicios) en el Centro de Excelencia para el desarrollo y la innovación CEDeI. Actualmente formo parte del equipo de integración de sistemas en el cliente Grupo Correos. Uso de herramientas IBM Integration Bus (WebSphere Message Broker).',
+                'skills' => 'Arquitectura Orientada a Servicios, IBM Integration Bus, WebSphere Message Broker'
+            ],
+            [
+                'date' => 'ago. 2018 - ago. 2019',
+                'location' => '38612 Granadilla, Santa Cruz de Tenerife',
+                'position' => 'Becario en ITER - Instituto Tecnológico y de Energías Renovables, S.A.',
+                'description' => 'Becario del programa CEDeI que promueve la creación de un plan de formación y empleo en el sector tecnológico, especialmente en el campo de las Tecnologías de la Información y la Innovación en Ciencia.',
+                'skills' => 'Tecnologías de la Información, Innovación en Ciencia'
             ]
         ];
 
