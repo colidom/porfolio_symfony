ace0055bb7a20cc5275f1e526e0129b3db216774
colidom <colidom@outlook.com>
2024-01-05 01:32:43 +0100

    Añadidos algunos cambios en los card de career

--- src/Controller/CareerController.php
+++ src/Controller/CareerController.php
@@ -19,7 +19,8 @@ class CareerController extends AbstractController
                 'company' => 'Eviden',
                 'modality' => 'En Remoto',
                 'description' => 'Desarrollo Web con Spring MVC, JSP e Hibernate en proyecto para el cliente EDP-Viesgo. Emocionado por lo que está por venir en este viaje de desarrollo web',
-                'skills' => 'Java, Spring MVC, JavaScript, Trabajo en equipo, Desarrollo de software, Apache, SQL'
+                'skills' => 'Java, Spring MVC, JavaScript, Trabajo en equipo, Desarrollo de software, Apache, SQL',
+                'image' => '/assets/img/companies/eviden.jfif'
             ],
             [
                 'date' => 'abr. 2023 - sept. 2023',
@@ -28,7 +29,8 @@ class CareerController extends AbstractController
                 'company' => 'Atos IT Solutions and Services A/S',
                 'modality' => 'Híbrido',
                 'description' => 'Mi labor consiste en desarrollar módulos de alta calidad para las plataformas e-commerce PrestaShop y Woocommerce, integrando servicios de paquetería nacional e internacional. Utilizo PHP, JavaScript, Python, SQL, servicios REST y SOAP, HTML5, CSS3, jQuery y Bootstrap',
-                'skills' => 'JavaScript, PHP, HTML, CSS, SQL, Desarrollo de software, Metodologías ágiles'
+                'skills' => 'JavaScript, PHP, HTML, CSS, SQL, Desarrollo de software, Metodologías ágiles',
+                'image' => '/assets/img/companies/atos.jfif'
             ],
             [
                 'date' => 'jun. 2021 - abr. 2023',
@@ -37,25 +39,28 @@ class CareerController extends AbstractController
                 'company' => 'Atos IT Solutions and Services A/S',
                 'modality' => 'Híbrido',
                 'description' => 'Desarrollo de módulo/plugin e-commerce en las plataformas PrestaShop y Woocommerce para el cliente Correos, integrando servicios de paquetería nacional e internacional. Tecnologías empleadas: PHP, JavaScript, Python, SQL, Servicios REST y SOAP, HTML5, CSS3, jQuery y Bootstrap',
-                'skills' => 'JavaScript, PHP, HTML, CSS, SQL, Desarrollo de software, Metodologías ágiles'
+                'skills' => 'JavaScript, PHP, HTML, CSS, SQL, Desarrollo de software, Metodologías ágiles',
+                'image' => '/assets/img/companies/atos.jfif'
             ],
             [
                 'date' => 'ago. 2019 - jun. 2021',
                 'location' => '38612 Granadilla, Santa Cruz de Tenerife',
                 'position' => 'Desarrollador SOA',
-                'company' => 'Centro de Excelencia para el desarrollo y la innovación CEDeI',
+                'company' => 'Instituto Tecnológico y de Energías Renovables, S.A',
                 'modality' => 'Presencial',
                 'description' => 'Desarrollador SOA (Arquitectura Orientada a Servicios) en el Centro de Excelencia para el desarrollo y la innovación CEDeI. Actualmente formo parte del equipo de integración de sistemas en el cliente Grupo Correos. Uso de herramientas IBM Integration Bus (WebSphere Message Broker)',
-                'skills' => 'Arquitectura Orientada a Servicios, IBM Integration Bus, WebSphere Message Broker'
+                'skills' => 'Arquitectura Orientada a Servicios, IBM Integration Bus, WebSphere Message Broker',
+                'image' => '/assets/img/companies/atos.jfif'
             ],
             [
                 'date' => 'ago. 2018 - ago. 2019',
                 'location' => '38612 Granadilla, Santa Cruz de Tenerife',
-                'position' => 'Becario en ITER - Instituto Tecnológico y de Energías Renovables, S.A',
-                'company' => 'Centro de Excelencia para el desarrollo y la innovación CEDeI',
+                'position' => 'Becario en ITER',
+                'company' => 'Instituto Tecnológico y de Energías Renovables, S.A',
                 'modality' => 'Presencial',
                 'description' => 'Becario del programa CEDeI que promueve la creación de un plan de formación y empleo en el sector tecnológico, especialmente en el campo de las Tecnologías de la Información y la Innovación en Ciencia',
-                'skills' => 'Arquitectura Orientada a Servicios, IBM Integration Bus, WebSphere Message Broker'
+                'skills' => 'Arquitectura Orientada a Servicios, IBM Integration Bus, WebSphere Message Broker',
+                'image' => '/assets/img/companies/cedei.jfif'
             ]
         ];
 
