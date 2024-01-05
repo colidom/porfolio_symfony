dc7840d0646307576bdd872853c61393eb36128d
colidom <colidom@outlook.com>
2024-01-05 16:36:13 +0100

    Actualizada plantilla y título

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,7 +12,7 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         return $this->render('home/home.html.twig', [
-            'title' => '¡Bienvenido a mi Porfolio!'
+            'title' => '¡Bienvenido/a, echa un vistazo!'
         ]);
     }
 }
