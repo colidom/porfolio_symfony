cfdedfe1171549262679cd37ca093efb086f1229
colidom <colidom@outlook.com>
2024-01-12 21:55:10 +0100

    Implementado send mail en vista contacto

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,30 +2,43 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
+
+// Asegúrate de tener este archivo creado en src/Form/ContactType.php
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact')]
-    public function index(): Response
+    public function index(Request $request, MailerInterface $mailer): Response
     {
-        $form = [];
+        $form = $this->createForm(ContactType::class);
 
-        if ('POST' === $_SERVER['REQUEST_METHOD']) {
+        $form->handleRequest($request);
 
-            $form = array(
-                "name" => $_POST['name'],
-                "email" => $_POST['email'],
-                "message" => nl2br($_POST["message"])
-            );
+        if ($form->isSubmitted() && $form->isValid()) {
+            $formData = $form->getData();
 
+            $email = (new Email())
+                ->from($formData['email'])
+                ->to('soporte@porfolio.com')
+                ->subject('Nuevo mensaje de contacto')
+                ->html($formData['message']);
+
+            $mailer->send($email);
+
+            // Ejemplo de redirección a una página de éxito:
+            return $this->redirectToRoute('contact_success');
         }
 
         return $this->render('contact/contact.html.twig', [
             'title' => 'Contacto',
-            'form' => $form
+            'form' => $form->createView()
         ]);
     }
 }
